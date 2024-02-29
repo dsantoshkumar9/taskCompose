ee5eb77ba5c57dec521ab225ac7607272b31ed39
dsantoshkumar9 <160123144+dsantoshkumar9@users.noreply.github.com>
2024-02-29 17:36:26 +0530

    hi abhinav

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 ARG NODE_VERSION=20.11.1
 FROM node:${NODE_VERSION}-alpine
 
-WORKDIR /app/data
+WORKDIR /app
 
-COPY . /app/data
+COPY . /app
 
 RUN npm install -g nodemon
 RUN npm install express
 RUN npm install fs
+RUN npm install mysql
 
 EXPOSE 3000
 
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const fs = require('fs');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
 
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'me',
+    password : 'password',
+    database : 'my_db'
+  });
 
 app.get('/sum', (req, res) => {
     const num1 = parseInt(req.query.num1);
@@ -12,34 +19,39 @@ app.get('/sum', (req, res) => {
 
     if (!isNaN(num1) && !isNaN(num2)) {
         const sum = num1 + num2;
-        var temp = (`The sum of ${num1} and ${num2} is ${sum}. \n`);
-        fs.appendFile('data.txt', temp, (err) => {
+        const query = `INSERT INTO sums (num1, num2, result) VALUES (${num1}, ${num2}, ${sum})`;
+        db.query(query, (err) => {
             if (err) {
-                console.error('Error appending to file:', err);
-                res.status(500).send('Error appending to file');
+                console.error('Error executing query:', err);
+                res.status(500).send('Error storing sum in database');
             } else {
-                console.log('Data appended to file successfully');
-                res.status(200).send(`${temp}`);
+                console.log('Sum stored in database successfully');
+                res.status(200).send(`The sum of ${num1} and ${num2} is ${sum}.`);
             }
         });
+        
     } else {
         res.status(400).send('Please provide valid numbers in query parameters.');
+        
     }
 });
 
 
-app.get('/all', (req,res)=>{
-    fs.readFile('data.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            res.status(500).send('Error reading file');
-        } else {
-            console.log("Data fetched");
-            res.status(200).send(`Data read from file:\n ${data}`);
-        }
+app.get('/all', (req, res) => {
+    const query = 'SELECT * FROM sums';
 
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error fetching sums from database');
+        } else {
+            console.log('Sums fetched from database');
+            const sums = results.map(({ num1, num2, result }) => `The sum of ${num1} and ${num2} is ${result}`).join('\n');
+            res.status(200).send(`All sums:\n${sums}`);
+        }
     });
-})
+});
+
 
 
 // http://localhost:3000/sum?num1=10&num2=12
